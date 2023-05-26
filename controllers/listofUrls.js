cfcec43689a6c662fe16566f6c1c060ffe314817
SAFI <exaevil@gmail.com>
2023-05-27 01:32:34 +0300

    modf2

--- controllers/listofUrls.js
+++ controllers/listofUrls.js
@@ -1,6 +1,6 @@
 // Handle GET request to display programs
 
-const urls = require("../models/urls");
+const { urls } = require("../models/");
 const getAllItems = async (req, res) => {
   const listofUrls = await urls.findAll();
   res.render("index", { programs: listofUrls });
