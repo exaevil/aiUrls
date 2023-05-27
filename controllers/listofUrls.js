8b9e7b8ebe1742eab57550c865aa3798bfc11974
SAFI <exaevil@gmail.com>
2023-05-27 12:09:07 +0300

    slight things

--- controllers/listofUrls.js
+++ controllers/listofUrls.js
@@ -1,8 +1,13 @@
-// Handle GET request to display programs
-
 const { urls } = require("../models/");
+
 const getAllItems = async (req, res) => {
+  // Fetch all items from the 'urls' model
   const listofUrls = await urls.findAll();
+
+  // Reverse the order of listofUrls
+  listofUrls.reverse();
+
+  // Render the 'index' template and pass the reversed listofUrls as the 'programs' variable
   res.render("index", { programs: listofUrls });
 };
 
